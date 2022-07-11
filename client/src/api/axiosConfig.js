5fff0cac8346525ff9916599877ed922deca0d3c
wassim triki <wsmtriki@gmail.com>
2022-07-11 16:10:03 +0100

    replace ur in axios config to /api

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
-  'https://app-chatup.herokuapp.com' || 'http://localhost:8080/api';
+const BASE_URL = '/api';
 
 const instance = axios.create({
   baseURL: BASE_URL,
