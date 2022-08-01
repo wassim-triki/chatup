4c61bbe9cbfc62f270184b7831b0c58b295eb28e
wassim triki <wsmtriki@gmail.com>
2022-08-01 18:31:48 +0100

    add profile pic compression before upload

---
+++ client/src/helpers/resizeFiles.js
@@ -0,0 +1,17 @@
+import Compress from 'compress.js';
+
+const resizeFiles = async (files, size, maxWidth, maxHeight) => {
+  const compress = new Compress();
+  const compressedFiles = await compress.compress(files, {
+    size,
+    quality: 1,
+    maxWidth,
+    maxHeight,
+    resize: true,
+  });
+  const resizedFiles = compressedFiles.map((f) =>
+    Compress.convertBase64ToFile(f.data, f.ext)
+  );
+  return resizedFiles.length === 1 ? resizedFiles[0] : resizedFiles;
+};
+export default resizeFiles;
--- client/src/helpers/uploadImage.js
+++ client/src/helpers/uploadImage.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
+import resizeFiles from './resizeFiles';
 
 const uploadImage = async (imageObj) => {
   const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME;
   const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
+
+  const resizedImage = await resizeFiles([imageObj], 4, 150, 150);
+
   const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
   const formData = new FormData();
   formData.append('upload_preset', UPLOAD_PRESET);
-  formData.append('file', imageObj);
+  formData.append('file', resizedImage);
   const resp = await axios.post(url, formData);
-
   return resp.data.url;
 };
 
